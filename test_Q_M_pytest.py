c266a73e9e38d1855848b881b36c79ee4dc741e9
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-09-08 01:39:32 +0530

    [Javee] test : TestCases Quantity Measurment of Feet_to_Inch and Inch_to_feet

--- test_Q_M_pytest.py
+++ test_Q_M_pytest.py
@@ -13,40 +13,83 @@ feets = quantity_measurment.Quantity_Measurment()
 inches = quantity_measurment.Quantity_Measurment()
 
 def test_feets_eqls_zero():
+    '''
+    Description: Function to test the feets by giving zero
+    Return: return Pass/Fail
+    '''    
     result = feets.calculate_feet(0)
     assert result == 0
 
 def test_feets_null_check():
+    '''
+    Description: Function to test the null
+    Return: return Pass/Fail
+    '''
     result = feets.calculate_feet(None)
     assert result == 'null'
 
 def test_feets_value_check():
+    '''
+    Description: Function to test the value of feets 
+    Return: return Pass/Fail
+    '''
     result = feets.calculate_feet(3)
     assert result == 36
 
 def test_feets_ref_check():
+    '''
+    Description: Function to test the reference
+    Return: return Pass/Fail
+    '''
     result = feets.calculate_feet(1)
     assert result == 12
 
 def test_feets_type_check():
+    '''
+    Description: Function to test the feets type
+    Return: return Pass/Fail
+    '''
     result = feets.type_of_input(1)
     assert result == 'int'
+
 '''================ Inch to feet ================='''
+
 def test_inch_equals_zero():
+    '''
+    Description: Function to test the inches by giving zero
+    Return: return Pass/Fail
+    '''
     result = inches.calculate_inch(0)
     assert result == 0
+
 def test_inch_null_check():
+    '''
+    Description: Function to test the null
+    Return: return Pass/Fail
+    '''
     result = inches.calculate_inch(None)
     assert result == None
 
 def test_inch_value_check():
+    '''
+    Description: Function to test the value
+    Return: return Pass/Fail
+    '''
     result = inches.calculate_inch(36)
     assert result == 3
 
 def test_inch_ref_check():
+    '''
+    Description: Function to test the reference
+    Return: return Pass/Fail
+    '''
     result = inches.calculate_inch(1)
     assert result == 0.08
 
 def test_inch_type_check():
+    '''
+    Description: Function to test the type of inch
+    Return: return Pass/Fail
+    '''
     result = inches.type_of_input(0)
     assert result == 'int'
